2905799907616703110d70169f6be95ea8093613
Landan <student@BCs-MacBook-Pro-3.local>
2022-02-14 10:15:05 -0600

    completed JS Exercise

---
+++ script.js
@@ -0,0 +1,32 @@
+// JS BASICS EXERCISE
+// 1
+console.log(3);
+
+// 2
+// Numbers, Strings, Booleans, Null, & Undefined
+// NOTE: Symbols are also a primitave. However, they are not clearly used
+
+// 3
+// Addition is +
+// Subtraction is -
+// Multiplication is *
+// Division is /
+// Exponentiation is **
+// Modulus is %
+
+// 4
+console.log(5 % 4); //1
+console.log(5 % 2); //1
+// console.log(5 % -4)
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye"); //Hawkeye
+
+// 7
+console.log("Landan".length + "Hester".length); //12
+
+// 8
+console.log("Avengers: Endgame"[16]); //16
